a01c4a4861498f983561f66214dc477542eb60be
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2023-01-03 15:27:45 -0600

    Imported dataset from db.json. Logged successfully

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import Gallery from './Gallery';
+import NavBar from './NavBar';
+import Home from './Home';
+import data from "./db.json"
 
 function App() {
-  const [posts, setPosts] = useState([]);
   // const [selectedPost, setSelectedPost] = useState("")
   // const [selectedProfile, setSelectedProfile] = useState("")
   // const [mostPopular, setMostPopular] = useState([])
   // const [filterByCategory, setFilterByCategory] = useState("")
 
-  useEffect(() => {
-    let url = "http://localhost:3000/posts";
-    fetch(url)
-      .then((r) => r.json())
-      .then(setPosts)
-  }, [])
-
+  console.log("Data: " , data)
+  console.log("Posts: " , data.posts)
   return (
     <div>
-      <Gallery posts={posts} />
+      <NavBar />
+      <Home />
     </div>
   );
 }
