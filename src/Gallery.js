d536836f02e76723c06c6b26f86930466b45fd68
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2023-01-03 16:57:24 -0600

    Implemented GET req to json db, compiled all data to state variables

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 
-function Gallery({ posts }){
 
+function Gallery(){
+    const [posts, setPosts] = useState([]);
+    const [comments, setComments] = useState([])
+// const [selectedPost, setSelectedPost] = useState("")
+  // const [selectedProfile, setSelectedProfile] = useState("")
+  // const [mostPopular, setMostPopular] = useState([])
+  // const [filterByCategory, setFilterByCategory] = useState("")
+
+    useEffect(() => {
+        fetch("db.json", {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
+          .then((r) => r.json())
+          .then((data) => compileData(data.posts, data.comments)
+          )
+      }, [])
+    
+    function compileData(posts, comments){
+        setPosts(posts)
+        setComments(comments)
+    }
+    
     const artworks = posts.map((post) => (
         <Post 
             key={post.id} 
