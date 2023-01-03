98b52d75e140964280eb29afd0949e6fa102f028
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2023-01-03 17:25:35 -0600

    Added Latest, MostPopular, NewArtwork, and PostPage components

---
+++ src/Latest.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Latest(){
+    return(
+        <div></div>
+    )
+}
+
+export default Latest
---
+++ src/NewArtwork.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NewArtwork(){
+    return(
+        <div></div>
+    )
+}
+
+export default NewArtwork
