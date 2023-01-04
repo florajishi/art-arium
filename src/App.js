299334d8b63c537cb97402bdbe788092df04b1c4
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2023-01-03 19:17:55 -0600

    Remove extra BrowserRouter tag, refine routing for react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import { Routes, Route, BrowserRouter } from "react-router-dom"
 import NavBar from './NavBar';
 import Home from './Home';
+import MostPopular from './MostPopular';
+import NewArtwork from './NewArtwork';
+import PostPage from './PostPage';
+import Latest from './Latest';
+import Gallery from './Gallery';
 
 function App() {
-  // const [selectedPost, setSelectedPost] = useState("")
-  // const [selectedProfile, setSelectedProfile] = useState("")
-  // const [mostPopular, setMostPopular] = useState([])
-  // const [filterByCategory, setFilterByCategory] = useState("")
+
+  const [page, setPage] = useState("/")
+  
+  function handleLinkClick(e) {
+    e.preventDefault()
+    setPage(e.target.pathname)
+  }
 
   return (
     <div>
-      <NavBar />
-      <Home />
+      <NavBar handleLinkClick={handleLinkClick}/>
+      <Gallery handleLinkClick={handleLinkClick}/>
+        <Routes>
+          <Route exact path='/latest' element={<Latest />} />
+          <Route exact path='/most-popular' element={<MostPopular />} />
+          <Route exact path='/submit-art' element={<NewArtwork />} />
+          <Route exact path='/view-post' element={<PostPage />} />
+          <Route exact path='/' element={<Home />} />
+        </Routes>
     </div>
   );
 }
