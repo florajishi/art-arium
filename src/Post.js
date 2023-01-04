4cb6a76bf3647900096f4f12b114323ebd3f901a
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2023-01-03 19:24:54 -0600

    Added temporary link formatting for posts

--- src/Post.js
+++ src/Post.js
@@ -1,16 +1,21 @@
 import React from "react";
 
-function Post({ post }){
+function Post({ post, handleLinkClick }){
     // need to add id to destructured prop
     const { title, artist, image, description, category, likes} = post
+
     return(
-        <div>
-            <h2>{title}</h2>
-            <img src={image} alt={title}/>
+        <div className="posts">
+            <a onClick={handleLinkClick} href="/view-art">
+                <h2>{title}</h2>
+                <img src={image} alt={title}/>
+            </a>   
             <h2>{artist}</h2>
             <p>Likes: {likes}</p>
             <p>{description}</p>
             <p>Category: {category}</p>
+            
+
         </div>
     )
 }
