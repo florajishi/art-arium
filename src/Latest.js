c87ba1d99b5fea7a82e728789815f014122a88c6
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2023-01-03 19:22:57 -0600

    Implemented GET request, compiled data for Post component. All code previously on Gallery component

--- src/Latest.js
+++ src/Latest.js
@@ -1,9 +1,38 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import Post from "./Post";
 
-function Latest(){
-    return(
-        <div></div>
-    )
+function Latest({ handleLinkClick }){
+    const [posts, setPosts] = useState([]);
+    const [comments, setComments] = useState([])
+
+    useEffect(() => {
+        fetch("db.json", {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
+          .then((r) => r.json())
+          .then((data) => compileData(data.posts, data.comments)
+          )
+      }, [])
+    
+    function compileData(posts, comments){
+        setPosts(posts)
+        setComments(comments)
+    }
+    
+    const artworks = posts.map((post) => (
+        <Post 
+            key={post.id} 
+            post={post}
+            handleLinkClick={handleLinkClick}
+        />
+    ))
+    return (
+        <div>{artworks}</div>
+    );
 }
+
 
 export default Latest
