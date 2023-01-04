7326058e789844a5086447762660ca0b9ddce4d6
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2023-01-03 19:20:12 -0600

    Cleaned up commented out code

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,7 @@ import React, { useEffect, useState } from "react";
 import Gallery from "./Gallery"
 
 function Home(){
-    // const [posts, setPosts] = useState([]);
-    
-    // useEffect(() => {
-    //     fetch("db.json", {
-    //         headers: {
-    //             'Accept': 'application/json',
-    //             'Content-Type': 'application/json',
-    //         }
-    //     })
-    //       .then((r) => r.json())
-    //       .then((json) => console.log("This is the response: " , json))
-    //   }, [])
-      
+
     return(
         <Gallery />
     )
