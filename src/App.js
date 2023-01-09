baeba81f9c91f4d14dbb8dd31d953c96d398fc32
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2023-01-09 17:44:26 -0600

    Refined all routing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import NavBar from './NavBar';
-import Home from './Home';
-import MostPopular from './MostPopular';
 import NewArtwork from './NewArtwork';
-import PostPage from './PostPage';
-import Latest from './Latest';
 import Gallery from './Gallery';
+import ArtworkDetails from './ArtworkDetails';
+
 
 function App() {
 
-  const [page, setPage] = useState("/")
-  
-  function handleLinkClick(e) {
-    e.preventDefault()
-    setPage(e.target.pathname)
-  }
-
   return (
-    <div>
-      <NavBar handleLinkClick={handleLinkClick}/>
-      <Gallery handleLinkClick={handleLinkClick}/>
-        <Routes>
-          <Route exact path='/latest' element={<Latest />} />
-          <Route exact path='/most-popular' element={<MostPopular />} />
-          <Route exact path='/submit-art' element={<NewArtwork />} />
-          <Route exact path='/view-post' element={<PostPage />} />
-          <Route exact path='/' element={<Home />} />
-        </Routes>
+    <div className='theme'>
+      <NavBar />
+      <Gallery />
+      <Routes>
+        <Route path='/latest' element={<Gallery />} />
+        <Route path='/most-popular' element={<Gallery />} />
+        <Route path='/submit-art' element={<NewArtwork />} />
+        <Route path='/view-art' element={<ArtworkDetails />} />
+        <Route path='/' element={<Navigate replace to='/latest' />} />
+      </Routes>
     </div>
   );
 }
